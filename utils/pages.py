6eaa201f990b8ca9a23c39f4eb3715122ac308a5
Robert Kern <me@robertkern.com>
2023-04-09 15:38:32 +1200

    Initial import

---
+++ utils/pages.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def set_page_config(title=""):
+    st.set_page_config(
+        page_title=title if len(title) > 0 else "PredictHQ Accommodation Demo App",
+        page_icon="🏨",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+            "Get help": "https://docs.predicthq.com",
+            "About": """
+                **PredictHQ Accommodation Demo App**
+
+                PredictHQ Technical Documentation can be found at [https://docs.predicthq.com](https://docs.predicthq.com).
+            """,
+        },
+    )
